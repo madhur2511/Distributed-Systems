192c17ff26bdd81900c73380036a24285f285fbf
= <madhur2511@gmail.com>
2016-04-02 20:16:12 -0700

    Adding Dockerfiles for CLient-server and Data Volume container, along with code changes in client-server logic

--- Cat/CatServer.java
+++ Cat/CatServer.java
@@ -5,29 +5,29 @@ public class CatServer{
 	public static void main(String [] args){
 		ServerSocket receiverSocket = null;
 		Socket ss = null;
-		
+
 		try{
 			BufferedReader br = null;
-			
+
 			// TODO: Handle args error separately
+
 			String filename = args[0];
 			int port = Integer.parseInt(args[1]);
 			receiverSocket = new ServerSocket(port);
 			ss = receiverSocket.accept();
 			DataInputStream in = new DataInputStream(ss.getInputStream());
    	    	DataOutputStream out = new DataOutputStream(ss.getOutputStream());
-			
+
 			System.out.println("Just connected to " + ss.getRemoteSocketAddress());
-		
 			while(true){
-   	    	    String req = in.readUTF();
+				String req = in.readUTF();
 				String line = null;
 				if(req.equals("LINE\n")){
 					if (br == null)
 						br = new BufferedReader(new FileReader(filename));
-					
+
 					line = br.readLine();
-					
+
 					if(line == null){
 						br.close();
 						br = new BufferedReader(new FileReader(filename));
@@ -40,14 +40,12 @@ public class CatServer{
 					System.out.println("Received: " + in.readUTF());
 				}
 
-        	}  
-		}catch(IOException e){
-			e.printStackTrace();
+        	}
+		}catch(Exception e){
 			try{
 				if(ss != null)	ss.close();
 				if(receiverSocket != null) receiverSocket.close();
 			}catch(IOException f){
-				f.printStackTrace();
 			}
 	    }
 	}
