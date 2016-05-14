892f710e26bdbbbfe6a1c8caf9a1564b88ad37d5
= <madhur2511@gmail.com>
2016-05-13 18:34:56 -0700

    Passing 18/18

--- Project2/naming/DfsObject.java
+++ Project2/naming/DfsObject.java
@@ -150,8 +150,13 @@ public class DfsObject {
         writersPending -= 1;
         currentWriters += 1;
         if(ftype == Ftype.FILE){
-            this.servers.removeAll(this.servers.subList(1, this.servers.size()));
-            this.commands.removeAll(this.commands.subList(1, this.commands.size()));
+            for(int i = this.servers.size() - 1; i > 0 ; i -= 1){
+                try{
+                    this.commands.get(i).delete(path);
+                }catch(Exception e){}
+                this.servers.remove(i);
+                this.commands.remove(i);
+            }
         }
     }
 
